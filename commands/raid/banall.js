53daedb8cebf8b7a49ea596499ff6eb02a83bc57
TechVevo <tech.vevo15@gmail.com>
2021-05-19 16:52:52 +0530

    Ban All Update

---
+++ commands/raid/banall.js
@@ -0,0 +1,41 @@
+const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
+
+module.exports = class KickAllCommand extends Commando.Command{
+    constructor(client){
+        super(client, {
+            name: 'banall',
+            memberName: 'banall',
+            group: 'raid',
+            description: 'Bans all bannable members',
+            clientPermissions: ['BAN_MEMBERS'],
+            throttling:{
+                usages: 1,
+                duration:60
+            }
+        })
+    }
+
+    async run(message){
+        message.delete()
+        console.info(`ENSURE THAT IN https://discord.com/developers/applications/${this.client.user.id}/bot, PRIVILEGED GATEWAY INTENTS > SERVRE MEMBERS INTENT IS "ON"`)
+        const {guild} = message
+        let firstmsg;
+        await message.channel.send('Banning all bannable members...').then(result => firstmsg=result)
+        
+        //Server Members Intent required under the Privileged Gateway Intents division
+        await guild.members.fetch().then(members => {
+            members.forEach(m => {
+                if(m.bannable){
+                    m.ban({reason: 'LOL'})
+                }
+            })
+        })
+
+        firstmsg.edit('Done!')
+
+        const moment = require('moment')
+        const time = moment().format("HH:mm:ss a")
+        console.log(`${time} | Command Ran: banall`)
+    }
+}
